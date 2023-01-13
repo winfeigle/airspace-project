6a33a2c8713149bc975bd80faf5fbe27468a377a
Win Feigle <win.feigle@gmail.com>
2023-01-13 08:24:16 -0700

    Add user uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    validates :username, presence: true
+    validates :username, presence: true, uniqueness: true
     validates :password, length: { minimum: 8 }
-    
+
     has_secure_password
 end

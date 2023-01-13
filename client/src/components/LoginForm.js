4e351bb526deb88bf9af929d1f563e7db49cfbe5
Win Feigle <win.feigle@gmail.com>
2023-01-13 12:04:06 -0700

    Add form values & state change

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -7,22 +7,28 @@ function LoginForm(){
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    console.log(`Username: ${username}, Password: ${password}`)
+
     return(
         <Form>
             <Form.Group className="mt-3 mb-3" controlId="formUsername">
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="username" placeholder="username" />
-                {/* <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-                </Form.Text> */}
+                <Form.Control 
+                    type="text" 
+                    placeholder="username" 
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="••••••••" />
-                {/* <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-                </Form.Text> */}
+                <Form.Control 
+                    type="password" 
+                    placeholder="••••••••" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
             </Form.Group>
 
             <Button variant="info" type="submit">

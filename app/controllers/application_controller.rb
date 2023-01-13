39a526202098c6cf0e2d771c4762997adfed5a13
Win Feigle <win.feigle@gmail.com>
2023-01-13 08:40:09 -0700

    Implement session update on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::API
 
   private
 
+  # @current_user = User.find_by(id: session[:user_id])
+
   def render_unprocessable_entity_response(invalid)
     render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end

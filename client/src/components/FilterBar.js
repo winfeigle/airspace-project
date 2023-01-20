ab674cea7c78f3a892f8a08954c8baeb2debf5d9
Win Feigle <win.feigle@gmail.com>
2023-01-20 10:56:19 -0700

    Add filter options

--- client/src/components/FilterBar.js
+++ client/src/components/FilterBar.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import FilterIcon from '../filter-icon.svg';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Form from 'react-bootstrap/Form';
 
 function FilterBar(){
+    const [filter, setFilter] = useState(null)
 
+    const handleFilter = (keyword) => {
+        setFilter(keyword)
+
+    }
 
     return(
         <div id="filter-search-bar">     
         <Dropdown id='dropdown' >
                 <Dropdown.Toggle variant="dark" id="dropdown-basic">
-                    <img id="filter-icon" alt="filter button" src={FilterIcon}/>  Filter
+                    <img id="filter-icon" alt="filter button" src={FilterIcon}/>  { filter ? filter : "Filter"}
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu>
-                    <Dropdown.Item href="#/price-highest">Price (high-low)</Dropdown.Item>
-                    <Dropdown.Item href="#/price-lowest">Price (low-high)</Dropdown.Item>
-                    <Dropdown.Item href="#/rating">Rating (highest)</Dropdown.Item>
+                    <Dropdown.Item 
+                        onClick={() => {handleFilter("price-high")}}
+                        href="#/price-highest">
+                            Price (high-low)</Dropdown.Item>
+                    <Dropdown.Item 
+                        onClick={() => {handleFilter("price-low")}}
+                        href="#/price-lowest">
+                            Price (low-high)</Dropdown.Item>
+                    <Dropdown.Item 
+                        onClick={() => {handleFilter("rating-high")}}
+                        href="#/rating">
+                            Rating (highest)</Dropdown.Item>
+                            <Dropdown.Item 
+                        onClick={() => {handleFilter(null)}}>
+                            <b>clear filters </b></Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
             <div className="vertical-seperator"></div>
